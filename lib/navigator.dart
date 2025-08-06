7812afb3d092afd75dfaf7cc9dd9cb8790c99f65
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-08-06 09:06:20 +0500

    navigator 1.0

---
+++ lib/navigator.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Navigator extends StatelessWidget {
+  const Navigator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+//  Navigator.push(
+//   context,
+//   MaterialPageRoute(builder: (context) => SecondPage()),
+// );
+  
+// navigator.push stack based approach use karta hai ismay screen new stack may add hoti hai like stack may screen ki oper neew screeen hi
+
+// Navigator.pop(context);
+// Navigator.pop(context); current stack may say screen ko remove kkarta hai oh
+    ],);
+  }
+}
